f0ca4df7e1492f1feaa1873c2965e3eb720dcd70
Andarcher <Hermosillodylan@gmail.com>
2023-01-31 12:33:51 -0700

    File Organization

---
+++ Python/#412 Fizz Buzz/FizzBuzz.py
@@ -0,0 +1,24 @@
+# FizzBuzz Solution
+# Description:
+# If a number is divisible by 3 print fizz, if divisible by 5 print buzz,
+# if divisible by both 3 and 5 print FizzBuzz
+
+
+
+for i in range(1, 101):
+
+    #FizzBuzz
+    if i%5==0 and i %3==0:
+        print("FizzBuzz")
+
+    #Fizz
+    elif i%3==0:
+        print("Fizz")
+
+    #Buzz
+    elif i%5==0:
+        print("Buzz")
+    
+    # If none of the above
+    else:
+        print(i)
