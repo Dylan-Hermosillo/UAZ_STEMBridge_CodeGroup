f7d5040ebaf2e8da0c38c5964657d8543b2ce143
Andarcher <113861407+Dylan-Hermosillo@users.noreply.github.com>
2023-02-04 09:32:24 -0700

    Add files via upload (#8)
    This is my javascript leet code solution to Palindrome Number.
    
    Co-authored-by: Seth Perritt <89667276+Durtyburd@users.noreply.github.com>

---
+++ isPalindrome.js
@@ -0,0 +1,15 @@
+// Palindrome Number
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+const isPalindrome = function (x) {
+  const str = String(x);
+  let count = 0;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] !== str[count++]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome(121), isPalindrome(-121), isPalindrome(123));
